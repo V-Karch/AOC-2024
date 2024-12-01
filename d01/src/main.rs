3fb26e0681810d8ac0333614c649126ed969ee2b
V-Karch <luna.karch05@gmail.com>
2024-12-01 15:04:45 -0500

    d01: added collection for each list

--- d01/src/main.rs
+++ d01/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    let contents = std::fs::read_to_string("input.txt")
+        .expect("Failed to read input file");
+
+    let part1_result = part1(&contents);
+}
+
+fn part1(contents: &String) -> i32 {
+    let left_list = collect_left_list(contents);
+    let right_list = collect_right_list(contents);
+    return 0; // Placeholder
+}
+
+fn collect_left_list(contents: &String) -> Vec<i32> {
+    return contents
+        .lines()
+        .filter_map(|line| line.get(..5))
+        .filter_map(|s| s.parse::<i32>().ok())
+        .collect();
+}
+
+fn collect_right_list(contents: &String) -> Vec<i32> {
+    return contents
+        .lines()
+        .filter_map(|line| line.get(8..))
+        .filter_map(|s| s.parse::<i32>().ok())
+        .collect();
 }
