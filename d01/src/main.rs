dcc27cd8fdd0084abc1901ed372db4acab40c844
V-Karch <luna.karch05@gmail.com>
2024-12-01 15:10:59 -0500

    d01: finished part 1

--- d01/src/main.rs
+++ d01/src/main.rs
@@ -3,12 +3,23 @@ fn main() {
         .expect("Failed to read input file");
 
     let part1_result = part1(&contents);
+    println!("Part 1: {}", part1_result);
 }
 
 fn part1(contents: &String) -> i32 {
-    let left_list = collect_left_list(contents);
-    let right_list = collect_right_list(contents);
-    return 0; // Placeholder
+    let mut left_list = collect_left_list(contents);
+    let mut right_list = collect_right_list(contents);
+
+    left_list.sort();
+    right_list.sort();
+
+    let mut sum = 0;
+
+    for index in 0..left_list.len() {
+        sum += (left_list[index] - right_list[index]).abs();
+    }
+
+    return sum;
 }
 
 fn collect_left_list(contents: &String) -> Vec<i32> {
