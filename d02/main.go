f807b373aa15799e824c086b020354749c8df39d
V-Karch <luna.karch05@gmail.com>
2024-12-02 00:13:52 -0500

    d02: read each line into an array of integers

--- d02/main.go
+++ d02/main.go
@@ -1,16 +1,46 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	contents, err := os.ReadFile("input.txt")
+	file, err := os.Open("input.txt")
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("Error opening file: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	var data [][]int
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		stringValues := strings.Fields(line)
+		var intValues []int
+		for _, str := range stringValues {
+			value, err := strconv.Atoi(str)
+			if err != nil {
+				fmt.Printf("Error converting '%s' to integer: %v\n", str, err)
+				return
+			}
+			intValues = append(intValues, value)
+		}
+		data = append(data, intValues)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
 		return
 	}
 
-	fmt.Println(string(contents))
+	fmt.Println("Parsed Data:")
+	for _, line := range data {
+		fmt.Println(line)
+	}
 }
