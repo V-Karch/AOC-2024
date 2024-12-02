5053a776c82a6507f462e675b06a5eb92ed599a9
V-Karch <luna.karch05@gmail.com>
2024-12-02 17:03:58 -0500

    d13: added base C# project to read lines

---
+++ d13/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Program {
+    static void Main() {
+        if (!File.Exists("input.txt")) {
+            Console.WriteLine("File not found.");
+            return;
+        }
+
+        List<string> lines = new List<string>(File.ReadAllLines("input.txt"));
+
+        foreach (string line in lines) {
+            Console.WriteLine(line);
+        }
+    }
+}
