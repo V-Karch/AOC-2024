457967a06408f5e46763ce19f2c7dc852275a9e1
V-Karch <luna.karch05@gmail.com>
2024-12-02 00:32:05 -0500

    d03: added template ruby file to just read the text input and display each line

--- d03/main.rb
+++ d03/main.rb
@@ -1,1 +1,12 @@
-puts "Hello World"
+file_name = "input.txt"
+
+begin
+    lines = File.read(file_name).split("\n")
+    lines.each do |line|
+        puts line
+    end
+rescue Errno::ENOENT
+    puts "Error: File '#{file_name}' not found."
+rescue => e
+    puts "An error occurred: #{e.message}"
+end
