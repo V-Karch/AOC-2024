6b429127d04bcf455a37d12bfdc4db175ec9339b
V-Karch <luna.karch05@gmail.com>
2024-12-03 12:41:07 -0500

    d03: added part 1 solution in ruby

--- d03/main.rb
+++ d03/main.rb
@@ -1,10 +1,26 @@
-begin
-    lines = File.read("input.txt").split("\n")
-    lines.each do |line|
-        puts line
+def part1(contents)
+    total_sum = 0
+    valid_mul_regex = /mul\((\d{1,3}),(\d{1,3})\)/ # REGEX PAIN OWIE
+    contents.each do |line|
+        matches = line.scan(valid_mul_regex)
+        total_sum += matches.sum { |x, y| x.to_i * y.to_i }
     end
-rescue Errno::ENOENT
-    puts "Error: File '#{file_name}' not found."
-rescue => e
-    puts "An error occurred: #{e.message}"
+
+    return total_sum
 end
+
+def main
+    begin
+        lines = File.read("input.txt").split("\n")
+
+        part1_result = part1(lines)
+        puts "Part 1: #{part1_result}"
+        
+    rescue Errno::ENOENT
+        puts "Couldn't read the input, womp womp"
+    rescue => e
+        puts "An error occurred: #{e.message}"
+    end
+end
+
+main
