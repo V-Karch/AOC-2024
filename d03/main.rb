24cf63b478b7f1a46725c08ebea5839a6382d08b
V-Karch <luna.karch05@gmail.com>
2024-12-03 12:55:50 -0500

    d03: added solution for part 2 in ruby

--- d03/main.rb
+++ d03/main.rb
@@ -9,13 +9,42 @@ def part1(contents)
     return total_sum
 end
 
+def part2(contents)
+    enable_regex = /do\(\)/
+    disable_regex = /don't\(\)/
+    mul_regex = /mul\((\d+),(\d+)\)/
+    is_enabled = true
+    fragments = contents.scan(/do\(\)|don't\(\)|mul\(\d+,\d+\)/)
+    # thank you https://regex101.com/ ;-;
+    sum = 0
+
+    fragments.each do |fragment|
+        case fragment
+        when enable_regex
+            is_enabled = true
+        when disable_regex
+            is_enabled = false
+        when mul_regex
+            if is_enabled
+            x, y = fragment.match(mul_regex).captures.map(&:to_i)
+            sum += x * y
+        end
+    end
+end
+    return sum
+end
+
+
 def main
     begin
-        lines = File.read("input.txt").split("\n")
+        lines = File.read("input.txt")
 
-        part1_result = part1(lines)
+        part1_result = part1(lines.split("\n"))
         puts "Part 1: #{part1_result}"
-        
+
+        part2_result = part2(lines)
+        puts "Part 2: #{part2_result}"
+
     rescue Errno::ENOENT
         puts "Couldn't read the input, womp womp"
     rescue => e
